bd320e779778e9e8a8d88bcb7955f6dbcdc346ed
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-10 18:12:25 -0300

    Get all without pagination

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from 'hono';
+import Character from '../db/characters.json';
 
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello! Hono!'))
+app.get('/all', (c) => c.json(Character))
 
 export default app
