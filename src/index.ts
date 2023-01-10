1331efbc6f0acd949af6ab81b613c50a00b4ffb4
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-10 18:26:36 -0300

    Add searchByName endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@ import Character from '../db/characters.json';
 const app = new Hono();
 
 app.get('/all', (c) => c.json(Character))
+app.get('/searchByName', (c) => {
+	const { q } = c.req.query();
+
+	if (!q || q.length < 3) {
+		return c.json({ error: 'Query must be at least 3 characters long' }, 400);
+	}
+
+	const reg = new RegExp(q, 'gi');
+	return c.json(Character.filter((char) => reg.test(char.name)))
+})
 
 export default app
