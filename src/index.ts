50b7dbef5be593a67e93d432fb6762601b28c297
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-11 10:38:29 -0300

    Format code

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import { Hono } from 'hono';
-import Character from '../db/characters.json';
-import { paginate } from './utils/paginate';
+import { Hono } from 'hono'
+import Character from '../db/characters.json'
+import { paginate } from './utils/paginate'
 
-const app = new Hono();
+const app = new Hono()
 
 /**
  * @api {get} /all Get all characters
  */
 app.get('/all', (c) => {
-	const { page: pageNumber, limit:  pageSize} = c.req.query()
-	return c.json(paginate(Character, {pageNumber, pageSize}))
+  const { page: pageNumber, limit: pageSize } = c.req.query()
+  return c.json(paginate(Character, { pageNumber, pageSize }))
 })
 
 /**
  * @api {get} /search Search for a character by it's name or charName
  */
 app.get('/search', (c) => {
-	const { q } = c.req.query();
+  const { q } = c.req.query()
 
-	if (!q || q.length < 3) {
-		return c.json({ error: 'Query must be at least 3 characters long' }, 400);
-	}
+  if (!q || q.length < 3) {
+    return c.json({ error: 'Query must be at least 3 characters long' }, 400)
+  }
 
-	const reg = new RegExp(q, 'gi');
-	return c.json(Character.filter((char) => reg.test(char.name) || reg.test(char.charName)))
+  const reg = new RegExp(q, 'gi')
+  return c.json(Character.filter((char) => reg.test(char.name) || reg.test(char.charName)))
 })
 
 export default app
--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,9 +1,9 @@
-export function paginate(items, {pageNumber, pageSize} ) {
-  const pageOrDefault = Number(pageNumber || 1);
-  const sizeOrDefault = Number(pageSize || 10);
+export function paginate (items, { pageNumber, pageSize }) {
+  const pageOrDefault = Number(pageNumber || 1)
+  const sizeOrDefault = Number(pageSize || 10)
 
   // If limit eq -1, then return all items
-  if (sizeOrDefault === -1) return {total: items.length, pages: 1, limit: sizeOrDefault, items}
+  if (sizeOrDefault === -1) return { total: items.length, pages: 1, limit: sizeOrDefault, items }
 
   // Return paginated items
   const startIndex = (pageOrDefault - 1) * sizeOrDefault
@@ -12,6 +12,6 @@ export function paginate(items, {pageNumber, pageSize} ) {
     total: items.length,
     pages: Math.ceil(items.length / sizeOrDefault),
     limit: sizeOrDefault,
-    items: itemsFiltered 
+    items: itemsFiltered
   }
 }
