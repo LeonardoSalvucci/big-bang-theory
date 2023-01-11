967ebafe05b3ef5975faa56e8f12a63992ad53a9
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-11 10:04:00 -0300

    Fix bug when limit eq -1

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,8 +1,11 @@
 export function paginate(items, {pageNumber, pageSize} ) {
   const pageOrDefault = Number(pageNumber || 1);
   const sizeOrDefault = Number(pageSize || 10);
-  
-  if (pageSize === -1) return {total: items.length, pages: 1, limit: items.length, items}
+
+  // If limit eq -1, then return all items
+  if (sizeOrDefault === -1) return {total: items.length, pages: 1, limit: sizeOrDefault, items}
+
+  // Return paginated items
   const startIndex = (pageOrDefault - 1) * sizeOrDefault
   const itemsFiltered = [...items].slice(startIndex, startIndex + sizeOrDefault)
   return {
