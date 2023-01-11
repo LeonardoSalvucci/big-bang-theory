1184cc93e153c91de443087fcb0dfebdfa85b089
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-11 12:20:52 -0300

    Customize tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
+      },
+      colors: {
+        'primary-orange': '#e67404',
+        'primary-light-orange': '#fce08c',
+        'bang-red': '#ae171a'
+      },
+      fontFamily: {
+        title: ['Anton', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
