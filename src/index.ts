fc3a205733027c6ad8eaa507d2520d586df01d90
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-11 09:27:41 -0300

    Modify search endpoint to include both name and charname

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import Character from '../db/characters.json';
 
 const app = new Hono();
 
+/**
+ * @api {get} /all Get all characters
+ */
 app.get('/all', (c) => c.json(Character))
-app.get('/searchByName', (c) => {
+
+/**
+ * @api {get} /search Search for a character by it's name or charName
+ */
+app.get('/search', (c) => {
 	const { q } = c.req.query();
 
 	if (!q || q.length < 3) {
@@ -12,17 +19,7 @@ app.get('/searchByName', (c) => {
 	}
 
 	const reg = new RegExp(q, 'gi');
-	return c.json(Character.filter((char) => reg.test(char.name)))
-})
-app.get('/searchByCharName', (c) => {
-	const { q } = c.req.query();
-
-	if (!q || q.length < 3) {
-		return c.json({ error: 'Query must be at least 3 characters long' }, 400);
-	}
-
-	const reg = new RegExp(q, 'gi');
-	return c.json(Character.filter((char) => reg.test(char.charName)))
+	return c.json(Character.filter((char) => reg.test(char.name) || reg.test(char.charName)))
 })
 
 export default app
