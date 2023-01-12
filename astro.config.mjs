31a9aca9a63ea07d5d898391175f7f04570e6f08
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-12 13:19:28 -0300

    Adding cloudflare adapter for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
+import cloudflare from '@astrojs/cloudflare'
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: cloudflare()
 })
