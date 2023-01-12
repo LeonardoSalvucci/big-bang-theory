36c6c7177694bdf70ca11409e80f6b653bf0892d
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-12 13:22:23 -0300

    Revert "Adding cloudflare adapter for deploy"
    This reverts commit 31a9aca9a63ea07d5d898391175f7f04570e6f08.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import cloudflare from '@astrojs/cloudflare'
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare()
+  output: 'server'
 })
